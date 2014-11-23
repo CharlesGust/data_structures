f9116b675bc6a605af57b455a562045d3ff810de
CharlesGust <charlesg@equality.net>
2014-11-23 11:30:26 -0800

    add insertion_sort and testing skeletons

---
+++ sort_insertion/insertion_sort.py
@@ -0,0 +1,23 @@
+import timeit
+
+# Assignment
+
+# When you're ready, create a new branch for your data-structures repository
+#  and add a module to implement the sorting algorithm.
+
+# Include an "if __name__ == '__main__':" block at the end of your module that
+#  demonstrates the performance characteristics of this sort over a variety of
+#  lengths of input in both the best and worst-case scenarios. Executing your
+#  module should print informative output about the performance of your sort to
+#  the terminal.
+
+# Add tests to demonstrate that the sorting algorithm works.
+
+# Add information about your implementation to the README.md in your package,
+#  including any sources and collaborations you used in creating it.
+
+def sort(unsorted):
+    pass
+
+if __name__=="__main__":
+
---
+++ sort_insertion/test_insertion_sort.py
@@ -0,0 +1,9 @@
+import py.test
+import unittest
+
+class testInsertionSort(unittest.TestCase):
+    def test__init__():
+        pass
+
+    def test_sort():
+        pass
